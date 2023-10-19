99fecc87d28029d32fb2ceb7e2dcb2b45f582397
Sean O'Connell <pinky@paperticketstudios.com>
2023-10-20 01:01:23 +1100

    Added new vids to WFC
    - Haven't looked back into SceneUtilities yet

--- Assets/Paperticket/Scripts/WFCControl.cs
+++ Assets/Paperticket/Scripts/WFCControl.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Video;
 using TMPro;
+using Paperticket;
 
 public class WFCControl : MonoBehaviour {
 
-    public VideoPlayer videoPlayer2D;
-    public VideoPlayer videoPlayer3D;
+    //public VideoPlayer videoPlayer2D;
+    //public VideoPlayer videoPlayer3D;
+
+    public VideoController videoController;
+
+    public VideoClip[] videoClips;
+    int videoClipsInt = 0;
 
     public List<GameObject> videoSpheres;
     int videoSphereInt = 0;
@@ -34,7 +40,7 @@ public class WFCControl : MonoBehaviour {
 
     bool wasPlaying = false;
 
-
+   
     void Start() {
         Debug.Log("[WFC Control] Hallo! This is a test message to make sure Development Mode is working :3 ");
 
@@ -52,71 +58,48 @@ public class WFCControl : MonoBehaviour {
         //if (!videoSpheres[0].activeInHierarchy) videoSpheres[0].SetActive(true); ;
 
         ToggleGrid();
+
+        StartCoroutine(StartFirstVideo());
+    }
+
+    IEnumerator StartFirstVideo() {
+        yield return new WaitForSeconds(0.5f);
+
+        videoController.videoPlayer.clip = videoClips[0];
+        videoController.videoPlayer.Play();
+
+
+        UpdateText();
+
     }
 
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) CycleSpheres();
         else if (Input.GetKeyDown(KeyCode.M)) ToggleSticks();
-        else if (Input.GetKeyDown(KeyCode.C)) ToggleCactus();
-        else if (Input.GetKeyDown(KeyCode.B)) ToggleBoard();
+        //else if (Input.GetKeyDown(KeyCode.C)) ToggleCactus();
+        //else if (Input.GetKeyDown(KeyCode.B)) ToggleBoard();
         else if (Input.GetKeyDown(KeyCode.G)) ToggleGrid();
         else if (Input.GetKeyDown(KeyCode.L)) ToggleLegs();
-        else if (Input.GetKeyDown(KeyCode.F)) ToggleParticles();
-        else if (Input.GetKeyDown(KeyCode.S)) ToggleScreen();
-        else if (Input.GetKeyDown(KeyCode.P)) ToggleVideo();
+        //else if (Input.GetKeyDown(KeyCode.F)) ToggleParticles();
+        //else if (Input.GetKeyDown(KeyCode.S)) ToggleScreen();
+        else if (Input.GetKeyDown(KeyCode.P)) ToggleVideoPlaying();
+        else if (Input.GetKeyDown(KeyCode.V)) CycleVideo();
     }
 
-    Coroutine cyclingSpheresCo;
-    void CycleSpheres() {
-        if (cyclingSpheresCo != null) return;
-        if (debugging) Debug.Log("[WFC Control] Cycling Spheres");
-        cyclingSpheresCo = StartCoroutine(CyclingSpheres());
-    }
-
-    IEnumerator CyclingSpheres() {
-
-        // Turn off current sphere
-        videoSpheres[videoSphereInt].SetActive(false);
-
-        // Pick the next sphere in the list
-        videoSphereInt = (videoSphereInt + 1) % videoSpheres.Count;
-
-        // Check whether the right video player is enabled
-        if ((!videoPlayer2D.gameObject.activeSelf && videoSpheres[videoSphereInt].GetComponent<Video3DHeadAdjust>() == null)||
-            (!videoPlayer3D.gameObject.activeSelf && videoSpheres[videoSphereInt].GetComponent<Video3DHeadAdjust>() != null)) {
-
-            if (debugging) Debug.Log("[WFC Control] Wrong video player active, attempting to switch video players...");
-            yield return SwitchingVideoPlayers();
-        }
-
-
-
-        if (debugging) Debug.Log("Switch check passed");
-
-        // Enable the new sphere
-        videoSpheres[videoSphereInt].SetActive(true);
-
-        // Send an event
-        if (cycleSphereEvent != null) cycleSphereEvent.Invoke();
-
-        // Change the text on the table
-        sphereText.text = "<b>Current Sphere</b> = '"+videoSpheres[videoSphereInt].name+"' ("+videoSphereInt+")";
-
-        cyclingSpheresCo = null;
-    }
+   
 
 
     void ToggleSticks() {
         measureSticks.SetActive(!measureSticks.activeSelf);
     }
 
-    void ToggleCactus() {
-        cactus.SetActive(!cactus.activeSelf);
-    }
-    void ToggleBoard() {
-        board.SetActive(!board.activeSelf);
-    }
+    //void ToggleCactus() {
+    //    cactus.SetActive(!cactus.activeSelf);
+    //}
+    //void ToggleBoard() {
+    //    board.SetActive(!board.activeSelf);
+    //}
 
     void ToggleGrid() {
         floorInt = Mathf.Abs(floorInt - 0.5f);
@@ -127,82 +110,157 @@ public class WFCControl : MonoBehaviour {
         legs.SetActive(!legs.activeSelf);
     }
 
-    void ToggleParticles() {
-        particles.SetActive(!particles.activeSelf);
-    }
-
-    void ToggleScreen() {
-        screen.SetActive(!screen.activeSelf);
-    }
-
-    void ToggleVideo() {
-
-        if (cyclingSpheresCo != null) {
-            Invoke("ToggleVideo", 0.1f);
-            Debug.LogWarning("[WFC Control] WARNING -> Tried to pause/play video while it was being cycles, waiting 0.1 seconds...");
-        }
-
-        var videoPlayer = videoPlayer2D.gameObject.activeSelf ? videoPlayer2D : videoPlayer3D;
-
-
-        if (videoPlayer.gameObject.activeInHierarchy) {
-        
-            if (videoPlayer.isPlaying) videoPlayer.Pause();
-            else videoPlayer.Play();
-
-        }
-
-        if (debugging) Debug.Log("[WFC Control] Toggled play/pause on " + videoPlayer.gameObject.name);
-
-    }
-
-
-    //public void SwitchVideoPlayers() {
-    //    Debug.Log("[WFC Control] Switching video players...");
-    //    StartCoroutine(SwitchingVideoPlayers());
+    //void ToggleParticles() {
+    //    particles.SetActive(!particles.activeSelf);
     //}
 
-    IEnumerator SwitchingVideoPlayers() {
+    //void ToggleScreen() {
+    //    screen.SetActive(!screen.activeSelf);
+    //}
 
-        var was2D = videoPlayer2D.gameObject.activeSelf;
-        var previousTime = was2D ? videoPlayer2D.time : videoPlayer3D.time;
+    void ToggleVideoPlaying() {
 
-        wasPlaying = was2D ? videoPlayer2D.isPlaying : videoPlayer3D.isPlaying;
-
-
-        //var player2Dtime = videoPlayer2D.time;
-        //var player3Dtime = videoPlayer3D.time;
-        
-        if (debugging) Debug.Log("[WFC Control] VideoPlayer2D time 1 = " + videoPlayer2D.time);
-        if (debugging) Debug.Log("[WFC Control] VideoPlayer3D time 1 = " + videoPlayer3D.time);
-
-
-        videoPlayer2D.gameObject.SetActive(!videoPlayer2D.gameObject.activeSelf);
-        videoPlayer3D.gameObject.SetActive(!videoPlayer3D.gameObject.activeSelf);
-        
-        //var is2D = videoPlayer2D.gameObject.activeSelf;
-        var videoPlayer = was2D ? videoPlayer3D : videoPlayer2D;
-
-        yield return new WaitUntil(() => videoPlayer.canSetTime);
-
-        videoPlayer.time = previousTime;
-        
-        //if (was2D) {
-        //    videoPlayer.time = player2Dtime % videoPlayer.clip.length;
-        //} else {
-        //    videoPlayer.time = (player3Dtime + 1) % videoPlayer.clip.length;
+        //if (cyclingSpheresCo != null) {
+        //    Invoke("ToggleVideoPlaying", 0.1f);
+        //    Debug.LogWarning("[WFC Control] WARNING -> Tried to pause/play video while it was being cycles, waiting 0.1 seconds...");
         //}
 
-        yield return null;
+        //var videoPlayer = videoPlayer2D.gameObject.activeSelf ? videoPlayer2D : videoPlayer3D;
 
-        if (debugging) Debug.Log("[WFC Control] VideoPlayer isPrepared 1 = " + videoPlayer.isPrepared);
-        yield return new WaitUntil(() => videoPlayer.isPrepared);
-        if (debugging) Debug.Log("[WFC Control] VideoPlayer isPrepared 2 = " + videoPlayer.isPrepared);
+        var vidPlayer = videoController.videoPlayer;
 
-        if (debugging) Debug.Log("[WFC Control] VideoPlayer2D time 2 = " + videoPlayer2D.time);
-        if (debugging) Debug.Log("[WFC Control] VideoPlayer3D time 2 = " + videoPlayer3D.time);
+        if (vidPlayer.gameObject.activeInHierarchy) {
+        
+            if (vidPlayer.isPlaying) vidPlayer.Pause();
+            else vidPlayer.Play();
 
-        if (!wasPlaying) videoPlayer.Pause();
+        }
+
+        if (debugging) Debug.Log("[WFC Control] Toggled play/pause on " + vidPlayer.gameObject.name);
 
     }
+
+
+
+    public void CycleVideo() {
+        Debug.Log("[BottleControl] Cycling video...");
+
+        videoClipsInt = (videoClipsInt + 1) % videoClips.Length;
+
+        videoController.videoPlayer.clip = videoClips[videoClipsInt];
+        videoController.videoPlayer.Play();
+
+        UpdateText();
+
+        //if (cycleVideoEvent && onCycleVideo != null) onCycleVideo.Invoke();
+    }
+
+    void UpdateText() {
+        sphereText.text = "<b>Current Sphere</b> = '" + videoSpheres[videoSphereInt].name + "' (" + videoSphereInt + ")" + System.Environment.NewLine
+                           + "<b>Current Video</b> = '" + videoClips[videoClipsInt].name + "' (" + videoClipsInt + ")";
+    }
+
+    //Coroutine cyclingSpheresCo;
+    void CycleSpheres() {
+        //if (cyclingSpheresCo != null) return;
+        if (debugging) Debug.Log("[WFC Control] Cycling Spheres");
+        //cyclingSpheresCo = StartCoroutine(CyclingSpheres());
+
+        // Turn off current sphere
+        videoSpheres[videoSphereInt].SetActive(false);
+
+        // Pick the next sphere in the list
+        videoSphereInt = (videoSphereInt + 1) % videoSpheres.Count;
+
+        // Enable the new sphere
+        videoSpheres[videoSphereInt].SetActive(true);
+
+        // Send an event
+        if (cycleSphereEvent != null) cycleSphereEvent.Invoke();
+
+        // Change the text on the table
+        sphereText.text = "<b>Current Sphere</b> = '" + videoSpheres[videoSphereInt].name + "' (" + videoSphereInt + ")";
+
+    }
+
+    //IEnumerator CyclingSpheres() {
+
+    //    // Turn off current sphere
+    //    videoSpheres[videoSphereInt].SetActive(false);
+
+    //    // Pick the next sphere in the list
+    //    videoSphereInt = (videoSphereInt + 1) % videoSpheres.Count;
+
+    //    //// Check whether the right video player is enabled
+    //    //if ((!videoPlayer2D.gameObject.activeSelf && videoSpheres[videoSphereInt].GetComponent<Video3DHeadAdjust>() == null) ||
+    //    //    (!videoPlayer3D.gameObject.activeSelf && videoSpheres[videoSphereInt].GetComponent<Video3DHeadAdjust>() != null)) {
+
+    //    //    if (debugging) Debug.Log("[WFC Control] Wrong video player active, attempting to switch video players...");
+    //    //    yield return SwitchingVideoPlayers();
+    //    //}
+    //    // if (debugging) Debug.Log("Switch check passed");
+
+    //    // Enable the new sphere
+    //    videoSpheres[videoSphereInt].SetActive(true);
+
+    //    // Send an event
+    //    if (cycleSphereEvent != null) cycleSphereEvent.Invoke();
+
+    //    // Change the text on the table
+    //    sphereText.text = "<b>Current Sphere</b> = '" + videoSpheres[videoSphereInt].name + "' (" + videoSphereInt + ")";
+
+    //    cyclingSpheresCo = null;
+    //}
+
+
+    ////public void SwitchVideoPlayers() {
+    ////    Debug.Log("[WFC Control] Switching video players...");
+    ////    StartCoroutine(SwitchingVideoPlayers());
+    ////}
+
+    //IEnumerator SwitchingVideoPlayers() {
+
+    //    var was2D = videoPlayer2D.gameObject.activeSelf;
+    //    var previousTime = was2D ? videoPlayer2D.time : videoPlayer3D.time;
+
+    //    wasPlaying = was2D ? videoPlayer2D.isPlaying : videoPlayer3D.isPlaying;
+
+
+    //    //var player2Dtime = videoPlayer2D.time;
+    //    //var player3Dtime = videoPlayer3D.time;
+
+    //    if (debugging) Debug.Log("[WFC Control] VideoPlayer2D time 1 = " + videoPlayer2D.time);
+    //    if (debugging) Debug.Log("[WFC Control] VideoPlayer3D time 1 = " + videoPlayer3D.time);
+
+
+    //    videoPlayer2D.gameObject.SetActive(!videoPlayer2D.gameObject.activeSelf);
+    //    videoPlayer3D.gameObject.SetActive(!videoPlayer3D.gameObject.activeSelf);
+
+    //    //var is2D = videoPlayer2D.gameObject.activeSelf;
+    //    var videoPlayer = was2D ? videoPlayer3D : videoPlayer2D;
+
+    //    yield return new WaitUntil(() => videoPlayer.canSetTime);
+
+    //    videoPlayer.time = previousTime;
+
+    //    //if (was2D) {
+    //    //    videoPlayer.time = player2Dtime % videoPlayer.clip.length;
+    //    //} else {
+    //    //    videoPlayer.time = (player3Dtime + 1) % videoPlayer.clip.length;
+    //    //}
+
+    //    yield return null;
+
+    //    if (debugging) Debug.Log("[WFC Control] VideoPlayer isPrepared 1 = " + videoPlayer.isPrepared);
+    //    yield return new WaitUntil(() => videoPlayer.isPrepared);
+    //    if (debugging) Debug.Log("[WFC Control] VideoPlayer isPrepared 2 = " + videoPlayer.isPrepared);
+
+    //    if (debugging) Debug.Log("[WFC Control] VideoPlayer2D time 2 = " + videoPlayer2D.time);
+    //    if (debugging) Debug.Log("[WFC Control] VideoPlayer3D time 2 = " + videoPlayer3D.time);
+
+    //    if (!wasPlaying) videoPlayer.Pause();
+
+    //}
+
+
 }
