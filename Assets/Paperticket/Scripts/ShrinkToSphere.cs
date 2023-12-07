64ed4caca6d419b5a6e4cc03c0e7523a99a36b76
Studio Kettle <itson@studiokettle.com.au>
2023-12-07 20:46:37 +1100

    Updated many models in WFC and Rock
    - ROCKARENA, RageSpiral, Reviews, Desk, ShootingStar

--- Assets/Paperticket/Scripts/ShrinkToSphere.cs
+++ Assets/Paperticket/Scripts/ShrinkToSphere.cs
@@ -9,7 +9,8 @@ public class ShrinkToSphere : MonoBehaviour {
 
     [SerializeField] float duration = 1.0f;
     [SerializeField] float scaleAmount = 0.1f;
-    [SerializeField] AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [SerializeField] AnimationCurve scaleCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [SerializeField] AnimationCurve constraintCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
     [Space(10)]
     [SerializeField] UnityEvent2 startEvents;
 
@@ -33,17 +34,36 @@ public class ShrinkToSphere : MonoBehaviour {
     IEnumerator Shrinking() {        
 
         if (startEvents != null) startEvents.Invoke();
+               
+        // Reset the position of the target to the sphere, but leave children where they are
+        var movement = constraint.GetSource(0).sourceTransform.position - transform.position;        
+        transform.position += movement;
+        foreach (Transform child in transform) {
+            child.position -= movement;
+        }
+        constraint.translationAtRest = transform.position;
+        constraint.rotationAtRest = transform.rotation.eulerAngles;
+
 
         // Scale the sphere down over (duration) seconds
-        StartCoroutine(PTUtilities.instance.ScaleTransformViaCurve(transform, curve, Vector3.one * scaleAmount, duration, TimeScale.Scaled));
+        StartCoroutine(PTUtilities.instance.ScaleTransformViaCurve(transform, scaleCurve, Vector3.one * scaleAmount, duration, TimeScale.Scaled));
 
         // Move the sphere to the target over (duration seconds)
+        constraint.SetTranslationOffset(0, Vector3.zero);
+        constraint.SetRotationOffset(0, Vector3.zero);
         constraint.constraintActive = true;
         float t = 0;
         while (constraint.weight != 1) {
-            t += duration * Time.deltaTime;
-            constraint.weight = Mathf.Clamp01(Mathf.Lerp(0, 1, curve.Evaluate(t)));
+            t += Time.deltaTime;
+            constraint.weight = Mathf.Clamp01(Mathf.Lerp(0, 1, constraintCurve.Evaluate(t / duration)));
             yield return null;
+        }
+
+        constraint.enabled = false;
+
+
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(false);
         }
 
     }
